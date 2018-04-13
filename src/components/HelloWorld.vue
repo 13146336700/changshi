ae2d066e0ab3049ad60c38712c0a1368b0ba9d01
571337259@qq.com <571337259@qq.com>
2018-04-13 16:39:25 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+  		
+  </div>
+</template>
+
+<script>
+import $ from 'jquery';
+import axios from 'axios';
+export default {
+  data () {
+    return {
+    	
+    }
+  },
+  created(){
+  },
+  mounted(){
+  	console.log( $("#inp").val())
+  },
+  methods:{
+  	
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
